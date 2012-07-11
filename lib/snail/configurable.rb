da7ca36dafe10b0bfcff8b36fc40e6a1bff4296f
Lance Ivy <lance@cainlevy.net>
2012-07-11 17:14:25 -0400

    raise Snail::UnknownAttribute if Snail doesn't recognize the key

--- lib/snail/configurable.rb
+++ lib/snail/configurable.rb
@@ -1,8 +1,15 @@
 class Snail
+  class UnknownAttribute < ArgumentError; end
+
   module Configurable
     def initialize(options = {}, &block)
       options.each do |k, v|
-        self.send("#{k}=", v)
+        m = "#{k}="
+        if respond_to? m
+          self.send(m, v)
+        else
+          raise UnknownAttribute.new(k)
+        end
       end
       yield self if block_given?
     end
